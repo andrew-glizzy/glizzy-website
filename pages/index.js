5b2e4d8a90ca6a893276231b33f1e057456c6d39
andrew-glizzy <andrew@getglizzy.com>
2022-02-05 16:23:28 -0600

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,12 @@ const Home = ({
     <div style={{ backgroundColor: colors.BACKGROUND }} className={styles.container}>
       <Head>
         <title>GLIZZY</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffe0ef" />
+        <meta name="msapplication-TileColor" content="#faf8f6"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0" />
         <meta name="description" content="Your favorite creators in the palm of your hand." />
         <meta name="theme-color" content={colors.BACKGROUND} />
